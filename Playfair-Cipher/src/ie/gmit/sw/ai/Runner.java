94c40c5d16fa84dab94a73ef61a3c5b149ca27cc
KyleFeng <kylezete@gmail.com>
2018-03-19 16:48:03 +0000

    basic simulatesAnnealing

--- Playfair-Cipher/src/ie/gmit/sw/ai/Runner.java
+++ Playfair-Cipher/src/ie/gmit/sw/ai/Runner.java
@@ -4,34 +4,36 @@ import java.util.Map;
 
 public class Runner {
     public static void main(String[] args) {
-        Decryptor de = new Decryptor();
-        char[][] cipher = new char[5][5];
-        // Add the key and the alphabet to the matrix
-        String key = "THEQUICKBROWNFXMPDVLAZYGS";
-        String cipher_text = "HEQEFIRCHITZMHUKOTXEDKWLHKHQVDSIEAKOZTXMTKOEEQSBXTDYHEUKUDBMKYZTFIRCEOMIYOZAEAMKIUZNQHTWDUOBVUDUPNOIEHEQKDLYWXNWILAZDYYOFTWAGADTVUDXXIEKITLKGKSIUYYOYETWDUOCHEFWHEKOABOKHUIMAREMWNFWFWIUNTTIOIOZAZTWFBRC";
-        int index = 0;
-        for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < 5; j++) {
-                if(index < key.length()) {
-                    cipher[i][j] = key.charAt(index);
-                    index++;
-                }
-            }
-        }
-        de.setKey(cipher);
-        de.setCipher_text(cipher_text);
-        String plain_text =  de.decrypt();
-        System.out.println(plain_text);
+//        Decryptor de = new Decryptor();
+//        char[][] cipher = new char[5][5];
+//        // Add the key and the alphabet to the matrix
+//        String key = "THEQUICKBROWNFXMPDVLAZYGS";
+        String cipher_text = "HEQEFIRCHITZMHUKOTXEDKWLHKHQVDSIEAKOZTXMTKOEEQSBXTDYHEUKUDBMKYZTFIRCEOMIYOZAEAMKIUZNQHTWDUOBVUDUPNOIEHEQKDLYWXNWILAZDYYOFTWAGADTVUDXXIEKITLKGKSIUYYOYETWDUOCHEFWHEKOABOKHUIMAREMWNFWFWIUNTTIOIOZAZTWFBRCHEXMTYDYHETIDTYOZRMAXWIUOITZMYDHBXHKUMSKXTDYMNXIMRNKZMXIHEXMHDTOOEKYSBQUKDOCHEZATCDEEKVUMXFCISGUYRFWRCOEEQUNZIIAKMVMQHEQMNXIWMKDKYWFIMTIQRUYTZDHPESMVUMRNKTIEXDKMSDQKSIWVOXIITRVQHEXDKPXOITWTHALNIHNOICZ";
+//        int index = 0;
+//        for(int i = 0; i < 5; i++) {
+//            for(int j = 0; j < 5; j++) {
+//                if(index < key.length()) {
+//                    cipher[i][j] = key.charAt(index);
+//                    index++;
+//                }
+//            }
+//        }
+//        de.setKey(cipher);
+//        de.setCipher_text(cipher_text);
+//        String plain_text =  de.decrypt();
+//        System.out.println(plain_text);
 
         ProbabilityCalculator cal = new ProbabilityCalculator("src/4grams.txt");
         Map<String,Double> grams = cal.get4Grams();
-        System.out.println("Probability(q) = "+cal.calLogProbability(grams.get("APPY")));
+//        System.out.println("Probability(q) = "+cal.calLogProbability(grams.get("APPY")));
+//
+//        System.out.println(new KeyGenerator().getRandomKey());
+//
+//        System.out.println(new KeyShuffler(cipher).shuffleKey());
+//
+//        System.out.println(HeuristicCalculator.getHeuristicValue(plain_text,grams));
 
-        System.out.println(new KeyGenerator().getRandomKey());
-
-        System.out.println(new KeyShuffler(cipher).shuffleKey());
-
-        System.out.println(HeuristicCalculator.getHeuristicValue(plain_text,grams));
+        System.out.println(new SimulatedAnnealing(cipher_text,grams).run());
 
     }
 }
---
+++ Playfair-Cipher/src/ie/gmit/sw/ai/SimulatedAnnealing.java
@@ -0,0 +1,57 @@
+package ie.gmit.sw.ai;
+
+import java.util.Map;
+
+public class SimulatedAnnealing {
+    private String cipher_text;
+    private String plain_text;
+    private Map<String,Double> grams;
+
+    public SimulatedAnnealing(String cipher_text, Map<String, Double> grams) {
+        this.cipher_text = cipher_text;
+        this.grams = grams;
+    }
+
+    public String run(){
+        // step 1
+        // Generate a random 25 letter key called parent
+        char[][] parent = new KeyGenerator().getRandomKey();
+
+        // step 2
+        // decrypt the cipher-text with the key
+        plain_text = new Decryptor(parent, cipher_text).decrypt();
+
+        // step 3
+        // get heuristic value of the key (logProbability)
+        Double score = HeuristicCalculator.getHeuristicValue(plain_text, grams);
+
+        // Starting Temperature = 10
+        // Final Temperature = 0
+        // Temperature Decrement = 1
+        for (int temp = 10; temp >= 0; temp--){
+            // Iterations at each temperature = 50000
+            for (int trainsitions = 50000; trainsitions >= 0; trainsitions--){
+                // make a small change to the key
+                char[][] child = new KeyShuffler(parent).shuffleKey();
+                // calculate child heuristic value
+                plain_text = new Decryptor(child, cipher_text).decrypt();
+                Double h = HeuristicCalculator.getHeuristicValue(plain_text, grams);
+                // get delta (h(child) - h(parent))
+                Double delta = h - score;
+                if (delta > 0){
+                    parent = child;
+                    score = h;
+                }else if (delta < 0){
+                    Double p = Math.exp(delta/temp);
+                    if (p > Math.random()){
+                        parent = child;
+                        score = h;
+                    }
+                }
+            }
+        }
+        // return the key
+        return KeyGenerator.convertToString(parent);
+    }
+
+}
